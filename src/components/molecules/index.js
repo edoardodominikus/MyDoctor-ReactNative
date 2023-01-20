82b74264bd229eb87b67f6c4c42c62e5e27d7ba0
Edoardo Dominikus, 蔣增豐 <79357023+edoardodominikus@users.noreply.github.com>
2023-01-20 14:43:30 +0800

    finished slicing messages screen

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -4,5 +4,6 @@ import HomeProfile from "./HomeProfile";
 import DoctorCategory from "./DoctorCategory";
 import RatedDoctors from "./RatedDoctors";
 import NewsItem from "./NewsItem";
+import ListDoctor from "./ListDoctor";
 
-export { Header, BottomNavigator, HomeProfile, DoctorCategory, RatedDoctors, NewsItem };
+export { Header, BottomNavigator, HomeProfile, DoctorCategory, RatedDoctors, NewsItem, ListDoctor };
